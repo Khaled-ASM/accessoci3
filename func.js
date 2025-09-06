702633f6b146f1754e5624c644d88cfedb0506d2
khaled-as <khaled_abdel_salam@hotmail.com>
2025-09-05 23:40:07 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -1,10 +1,26 @@
-const fdk=require('@fnproject/fdk');
+const fdk			= require('@fnproject/fdk');
+const common        = require("oci-common");
+const identity      = require("oci-identity");
 
-fdk.handle(function(input){
-  let name = 'World';
-  if (input.name) {
-    name = input.name;
-  }
-  console.log('\nInside Node Hello World function')
-  return {'message': 'Hello ' + name}
+fdk.handle(async function(input, ctx){
+	//try {
+		let jiraClientId	= 0;
+		let jiraClientSecret= 0;
+		const provider = common.ResourcePrincipalAuthenticationDetailsProvider.builder();
+		//const provider 		= new common.ResourcePrincipalAuthenticationDetailsProvider();
+		//(async () => {
+		//const identityClient 	  = await new identity.IdentityClient({authenticationDetailsProvider: provider});
+		
+		//const provider 			= new common.ResourcePrincipalAuthenticationDetailsProvider();
+        //const identityClient 	= new identity.IdentityClient({ authenticationDetailsProvider: provider });
+		const ociVaultName        = "IBA Vault";
+		const ociCompartmentName  = "NONPROD-Oracle-Integration-CMP";
+		console.log("TenantId: " + provider.getTenantId())
+		console.log("Check log Printed or not !!!!");
+		//})();
+		return {"tenantId": provider.getTenantId()}; //provider.getTenantId()
+	//} catch (error) {
+	//	console.error('Error fetching data:', error);
+	//	return error;
+	//}
 })
